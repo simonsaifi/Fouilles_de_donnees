b1c1f86dc2be934dc6c646a1ab2d14f6ce5a1051
A914152 <simon.elkhouryelsaifi@atos.net>
2024-03-11 10:35:32 +0100

    Modif Weights

--- sommes_ponderee.py
+++ sommes_ponderee.py
@@ -17,8 +17,15 @@ for col in data.columns:
     # Pas besoin de traiter 'Prix' différemment ici car il est déjà dans 'minimize_columns'
 
 # Appliquer des poids égaux et calculer le score
-# Note : Ajustez les poids selon vos besoins
-weights = {col: 1 for col in data.columns if col != 'Score'}
+weights = {
+    'Prix': 0.25,
+    'Vitesse_Max': 0.15,
+    'Conso_Moy': 0.1,
+    'Dis_Freinage': 0.1,
+    'Confort': 0.1,
+    'Vol_Coffre': 0.2,
+    'Acceleration': 0.1
+}
 data['Score'] = sum(data[col] * weights[col] for col in weights.keys())
 
 # Trier les données par score en ordre décroissant pour voir les meilleures options en haut
