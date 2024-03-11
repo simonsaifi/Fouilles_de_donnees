9be13742c3277bc96d0552c9a5e07e7ee7c9a958
A914152 <simon.elkhouryelsaifi@atos.net>
2024-03-11 09:41:13 +0100

    Mini Maxi

--- Min-Max.py
+++ Min-Max.py
@@ -7,11 +7,18 @@ data = pd.read_csv(file_path)
 
 # Définir les critères à minimiser
 minimize_columns = ['Dis_Freinage', 'Acceleration']
+maximize_columns = ['Vitesse_Max', 'Confort', 'Vol_Coffre']
 
 # Inverser les valeurs pour les critères à minimiser
 for column in minimize_columns:
     data[column] = data[column].max() - data[column] + data[column].min()
 
+# Maximisation
+for column in maximize_columns:
+    data[column] = (data[column] - data[column].min()) / (data[column].max() - data[column].min())
+
+
 # Vous pouvez maintenant sauvegarder le nouveau DataFrame modifié ou travailler avec celui-ci
-output_file_path = 'donnees_minimiser.csv'
+output_file_path = 'donnees_min-max.csv'
 data.to_csv(output_file_path, index=False)
+print(f'Les données ont été sauvegardées dans {output_file_path}')
